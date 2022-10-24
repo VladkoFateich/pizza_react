32c2a525ecf5b819d37001c7171dd277876a46e8
Vladislav Fateev <Fateev_wbd@mail.ru>
2022-10-24 13:20:30 +0500

    Cart

--- src/App.js
+++ src/App.js
@@ -6,21 +6,18 @@ import { Routes, Route } from "react-router-dom";
 
 import { Home } from "./pages/Home";
 import { NotFound } from "./pages/NotFound";
-import {Cart} from './pages/Cart'
-
+import { Cart } from "./pages/Cart";
 
 function App() {
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,13 @@ import { Content } from "../components/Content";
 
 export const Home = () => {
     return (
-        <>
+        <div className="container">
         <div className="content__top">
         <Categories />
         <Sort />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <Content/>
-      </>
+      </div>
     )
 }
